a8df7da72683fbac51864ba355f3776f8df739f9
Gustavo Bessa <gbvirtual@gmail.com>
2018-12-03 08:41:42 -0200

    Taking picture and showing in a thumbnail

--- app/src/main/java/br/com/gbessa/logmyhaircuts/MainActivity.kt
+++ app/src/main/java/br/com/gbessa/logmyhaircuts/MainActivity.kt
@@ -1,12 +1,41 @@
 package br.com.gbessa.logmyhaircuts
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_launch_camera.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
     }
+
+    /**
+     *  Source: https://developer.android.com/training/camera/photobasics#kotlin
+     */
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data.extras.get("data") as Bitmap
+            image_from_camera.setImageBitmap(imageBitmap)
+        }
+    }
+
 }
